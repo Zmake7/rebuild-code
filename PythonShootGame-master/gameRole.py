aecadf7e79e3b4927e204bae5f94ca88a29f1cb3
Zmake7 <zhoutiancheng84@gmail.com>
2023-06-22 11:00:47 +0700

    Revert "成功"
    This reverts commit 91fddbe7da5205680ebb65eddaa7be196a0af3fd.

--- PythonShootGame-master/gameRole.py
+++ PythonShootGame-master/gameRole.py
@@ -15,7 +15,16 @@ TYPE_MIDDLE = 2
 TYPE_BIG = 3
 
 # 子弹类
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, bullet_img, init_pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullet_img
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = init_pos
+        self.speed = 10
 
+    def move(self):
+        self.rect.top -= self.speed
 
 # 玩家类
 class Player(pygame.sprite.Sprite):
